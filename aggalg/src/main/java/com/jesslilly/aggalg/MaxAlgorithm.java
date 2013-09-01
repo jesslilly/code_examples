54d7baa1d797b4572b2ad304465cbd3e6f623ccb
jmlilly <jmlilly@us.ibm.com>
2013-08-31 22:00:06 -0400

    Implement MaxAlgorithm

--- aggalg/src/main/java/com/jesslilly/aggalg/MaxAlgorithm.java
+++ aggalg/src/main/java/com/jesslilly/aggalg/MaxAlgorithm.java
@@ -3,20 +3,55 @@
  */
 package com.jesslilly.aggalg;
 
+import java.util.List;
+
 /**
  * @author jmlilly
  *
  */
 public class MaxAlgorithm extends AggregateAlgorithmImpl {
 	
-	private Double maxValue;
+	private Double max;
+	
+	public MaxAlgorithm() {
+		super();
+		max = null;
+	}
+
+	@Override
+	public void initialize(List<Double> numbers) {
+		for (Double number : numbers) {
+			append(number);
+		}
+	}
+
+	@Override
+	public void append(Double number) {
+		super.append(number);
+		if (max == null || number > max) {
+			max = number;
+		}
+	}
+
+	@Override
+	public void append(List<Double> numbers) {
+		for (Double number : numbers) {
+			append(number);
+		}
+	}
 
 	/* (non-Javadoc)
 	 * @see com.jesslilly.aggalg.AggregateAlgorithm#aggregate()
 	 */
 	public Double aggregate() {
-		// TODO Auto-generated method stub
-		return null;
+		return getMax();
+	}
+
+	/**
+	 * @return the max
+	 */
+	public Double getMax() {
+		return max;
 	}
 
 }
